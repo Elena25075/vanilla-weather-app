4eb7a1abe2ef0722aef58ddda835cd95df7aa581
Elena Kartoshkina <96824172+Elena25075@users.noreply.github.com>
2022-08-04 21:00:31 +0300

    API key fixed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 let apiKey = "fd796e85e2a0e40f557a4de490967886";
 let cityName = "Lviv";
 let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey)";
+  "https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}";
 console.log(apiUrl);
